6a4c81973c591b3476e2a26d3d69f7029ea15d3c
iasfilho <123alves@gmail.com>
2025-11-23 22:55:06 -0300

    Att, BackEnd

--- back_end/app/views.py
+++ back_end/app/views.py
@@ -1,6 +1,6 @@
 from .models import Message
 from .serializers import MessageSerializer
-from rest_framework import viewsets, status
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
